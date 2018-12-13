2d8ea35e71ca4c57c3007babadb63d65f27cfeee
shannonkjones <shankj687@gmail.com>
2018-12-13 14:46:59 -0500

    Added crispy form styling to signup and login

--- pops_website/settings/base.py
+++ pops_website/settings/base.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
