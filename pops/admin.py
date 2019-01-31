cdcf7a8716d3f366876e01702bb5b23bf0963943
shannonkjones <shankj687@gmail.com>
2019-01-31 09:58:56 -0500

    Updated admin to not overwrite user when saving changes to case study

--- pops/admin.py
+++ pops/admin.py
@@ -6,7 +6,9 @@ class CaseStudyAdmin(admin.ModelAdmin):
     list_display = ('name', 'number_of_pests', 'number_of_hosts', 'start_year', 'end_year', 'time_step', 'created_by', 'date_created')
 
     def save_model(self, request, obj, form, change):
-        obj.created_by = request.user
+        if not obj.created_by:
+            obj.created_by = request.user
+        obj.save()
 
         return super(CaseStudyAdmin, self).save_model(request, obj, form, change)
 
