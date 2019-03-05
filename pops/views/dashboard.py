1bfb0dfbaf5f7dedcc8ca44a07bb1f83194ca641
shannonkjones <shankj687@gmail.com>
2019-03-05 10:41:50 -0500

    Updated dashboard layout

--- pops/views/dashboard.py
+++ pops/views/dashboard.py
@@ -16,7 +16,7 @@ class WorkspaceView(TemplateView):
             return context
 
 class DashboardView(TemplateView):
-    template_name = 'pops/dashboard/dashboard.html'
+    template_name = 'pops/dashboard/dashboard_layout.html'
 
     def get_context_data(self, **kwargs):
             # Call the base implementation first to get the context
