533c3fbc085f6df1f3b4634d5065a65439f48943
shannonkjones <shankj687@gmail.com>
2020-02-18 15:57:35 -0500

    Added shared sessions to workspace view

--- pops/views/dashboard.py
+++ pops/views/dashboard.py
@@ -11,7 +11,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.csrf import ensure_csrf_cookie
 
 from django.db.models.functions import Greatest
-from django.db.models import Prefetch, Sum, Count, Max, Min, OuterRef, Subquery, Q
+from django.db.models import Prefetch, Sum, Count, Exists, Max, Min, OuterRef, Subquery, Q
 
 from ..models import *
 from ..forms import *
@@ -88,8 +88,9 @@ class WorkspaceView(LoginRequiredMixin,TemplateView):
             current_user=self.request.user
             context['current_user']=current_user
             context['user_case_studies'] = CaseStudy.objects.filter(created_by = current_user).order_by('-date_created')[:5]
-            context['user_sessions'] = Session.objects.annotate(number_runs=Count('runcollection')).annotate(most_recent_run=Max('runcollection__date_created')).prefetch_related('created_by','case_study').filter(created_by = self.request.user).order_by('-date_created')[:5]
-            context['number_of_sessions'] = Session.objects.filter(created_by = current_user).count() 
+            #context['user_sessions'] = Session.objects.annotate(number_runs=Count('runcollection')).annotate(most_recent_run=Max('runcollection__date_created')).prefetch_related('created_by','case_study').filter(created_by = self.request.user).order_by('-date_created')[:5]
+            context['shared_sessions'] = Session.objects.prefetch_related('created_by','case_study').filter(Q(created_by = current_user ) | Q(allowedusers__user=current_user)).annotate(shared=Count('allowedusers',distinct=True)).annotate(number_runs=Count('runcollection', distinct=True)).annotate(most_recent_run=Max('runcollection__date_created')).order_by('-date_created')[:5]
+            context['number_of_sessions'] = Session.objects.filter(Q(created_by = current_user ) | Q(allowedusers__user=current_user)).count() 
             return context
 
 class SessionListView(LoginRequiredMixin, TemplateView):
