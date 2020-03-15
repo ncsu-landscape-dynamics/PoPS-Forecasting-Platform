e58c5cb2416d2b8c53ad14565d930d9e240887f4
shannonkjones <shankj687@gmail.com>
2020-03-15 10:53:59 -0400

    Added basic tests for static pages and CustomUser pages

---
+++ pops/tests/test_basic_pages.py
@@ -0,0 +1,52 @@
+from django.test import TestCase
+from django.urls import reverse
+
+class LandingPageViewTest(TestCase):
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+           
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('temp_landing'))
+        self.assertEqual(response.status_code, 200)
+
+class FAQPageViewTest(TestCase):
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/faq/')
+        self.assertEqual(response.status_code, 200)
+           
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('FAQs'))
+        self.assertEqual(response.status_code, 200)
+
+class TutorialsPageViewTest(TestCase):
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/tutorials/')
+        self.assertEqual(response.status_code, 200)
+           
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('tutorials'))
+        self.assertEqual(response.status_code, 200)
+        
+class TermsAndConditionsPageViewTest(TestCase):
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/terms_and_conditions/')
+        self.assertEqual(response.status_code, 200)
+           
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('terms_and_conditions'))
+        self.assertEqual(response.status_code, 200)
+ 
+class PrivacyPolicyPageViewTest(TestCase):
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/privacy_policy/')
+        self.assertEqual(response.status_code, 200)
+           
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('privacy_policy'))
+        self.assertEqual(response.status_code, 200)
