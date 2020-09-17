980ccb2d6e56b95e3f70e07d2d5284d00ffc6489
shannonkjones <shankj687@gmail.com>
2020-09-17 11:32:56 -0400

    Updated gauge plot to be more dynamic (display 'thousands','millions', etc.)

--- static_custom/js/dashboard/gaugePlot.js
+++ static_custom/js/dashboard/gaugePlot.js
@@ -1,18 +1,44 @@
 function gaugePlot(div_id, host_removal_dollars, pesticide_dollars, max_amount) {
 
-  var max_level = max_amount/1000000;
-  var host_removal_cost = host_removal_dollars/1000000;
-  var pesticide_cost = pesticide_dollars/1000000;
-  var total_cost = host_removal_cost + pesticide_cost;
+  var total_cost_dollars = host_removal_dollars + pesticide_dollars;
 
-  if (total_cost >= max_level) {
+  function divideCost(amount,divide_by) {
+    new_cost = round(amount/divide_by,1);
+    console.log('New function works: ' + new_cost);
+    return new_cost
+  }
+
+    var divide_by = 1;
+    var display_unit = '';
+
+  if (total_cost_dollars >= 1e9) {
+    divide_by = 1e9;
+    display_unit = 'billion';
+  }
+  else if (total_cost_dollars >= 1e6) {
+    divide_by = 1e6;
+    display_unit = 'million';
+  }
+  else if (total_cost_dollars >= 1e3) {
+    divide_by = 1000;
+    display_unit = 'thousand';
+  }
+
+  var max_level = divideCost(max_amount,divide_by);
+  var host_removal_cost = divideCost(host_removal_dollars,divide_by);
+  var pesticide_cost = divideCost(pesticide_dollars,divide_by);
+  var total_cost = divideCost(total_cost_dollars,divide_by);
+
+
+  if (total_cost_dollars >= max_amount) {
     main_color = "#f55949";
-    drawn_level = max_level;
-    var overbudget = total_cost - max_level;
-    warning_msg = "Budget exceeded by:<br>$" + round(overbudget, 2) + " million";
+    drawn_level = max_amount;
+    var overbudget = total_cost_dollars - max_amount;
+    var displayed_overbudget = overbudget.toLocaleString("en");
+    warning_msg = "Budget exceeded by:<br>$" + displayed_overbudget + " " ;
     warning_color = "#f55949";
   var data = [{
-    values: [max_level,(0.98 * max_level - total_cost), (max_level / 50), (max_level / 3)],
+    values: [max_amount,0, (max_amount / 50), (max_amount / 3)],
     rotation: -135,
     direction: "clockwise",
     sort: false,
@@ -21,8 +47,8 @@ function gaugePlot(div_id, host_removal_dollars, pesticide_dollars, max_amount) 
     marker: {
       colors: [main_color, '#676b73', '#f55949', 'rgba(255, 255, 255, 0)']
     },
-    labels: ['Total <br> cost:<br>$' + round(total_cost, 3) + ' million', '', 'Budget: $' + max_level +
-      ' million', ''
+    labels: ['Total <br> cost:<br>$' + total_cost + ' ' + display_unit, '', 'Budget: $' + max_level +
+      ' '+ display_unit, ''
     ],
     hoverinfo: 'label',
     hole: 0.8,
@@ -31,11 +57,11 @@ function gaugePlot(div_id, host_removal_dollars, pesticide_dollars, max_amount) 
   }];
         } else {
     main_color = "#7ba83d";
-    drawn_level = total_cost;
+    drawn_level = total_cost_dollars;
     warning_msg = 'spent on<br>management';
     warning_color = 'white';
   var data = [{
-    values: [host_removal_cost, pesticide_cost,(0.98 * max_level - total_cost), (max_level / 50), (max_level / 3)],
+    values: [host_removal_dollars, pesticide_dollars,(0.98 * max_amount - total_cost_dollars), (max_amount / 50), (max_amount / 3)],
     rotation: -135,
     direction: "clockwise",
     sort: false,
@@ -44,9 +70,9 @@ function gaugePlot(div_id, host_removal_dollars, pesticide_dollars, max_amount) 
     marker: {
       colors: [main_color, "a3e04f", '#676b73', '#f55949', 'rgba(255, 255, 255, 0)']
     },
-    labels: ['Host removal <br> cost:<br>$' + round(host_removal_cost, 3) + ' million',
-    'Pesticide <br> cost:<br>$' + round(pesticide_cost, 3) + ' million', '', 'Budget: $' + max_level +
-      ' million', ''
+    labels: ['Host removal <br> cost:<br>$' + host_removal_cost + ' ' + display_unit,
+    'Pesticide <br> cost:<br>$' + pesticide_cost + ' ' + display_unit, '', 'Budget: $' + max_level +
+      ' ' + display_unit, ''
     ],
     hoverinfo: 'label',
     hole: 0.8,
@@ -61,12 +87,12 @@ function gaugePlot(div_id, host_removal_dollars, pesticide_dollars, max_amount) 
         yref: 'paper',
         x: 0.5,
         xanchor: 'middle',
-        y: 0.5,
+        y: 0.55,
         yanchor: 'middle',
-        text: '$' + round(total_cost, 1),
+        text: '$' + total_cost,
         font: {
           family: 'Helvetica',
-          size: 30,
+          size: 28,
           color: '#ffffff'
         },
         align: 'center',
@@ -78,12 +104,12 @@ function gaugePlot(div_id, host_removal_dollars, pesticide_dollars, max_amount) 
         yref: 'paper',
         x: 0.5,
         xanchor: 'center',
-        y: 0.3,
+        y: 0.35,
         yanchor: 'middle',
-        text: 'million',
+        text: display_unit,
         font: {
           family: 'Helvetica',
-          size: 20,
+          size: 18,
           color: '#ffffff'
         },
         align: 'center',
@@ -100,7 +126,7 @@ function gaugePlot(div_id, host_removal_dollars, pesticide_dollars, max_amount) 
         text: warning_msg,
         font: {
           family: 'Helvetica',
-          size: 14,
+          size: 12,
           color: warning_color,
         },
         align: 'center',
