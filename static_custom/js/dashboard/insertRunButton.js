14584c01ec768de007ff9d099523962977e97ef6
Shannon Jones <30442313+shannonkjones@users.noreply.github.com>
2020-09-21 06:24:18 -0400

    Bugfix/clean up dashboard code (#101)
    * Created external functions for updateDrawColor, run_comparison_plot, and insertRunButton
    
    * Removed external functions from dashboard.html
    
    * Formatted HTML of dashboard.html
    
    * Moved CircleMode, gaugePlot and setDrawColor to external functions
    
    * Updated dashboard with external functions
    
    * Moved mapbox layer functions and spread rate plot function external
    
    * Updated mapbox drawn layers and side bar plots to be separate functions
    
    * Added cost to the drawn polygons
    
    * remove updateArea() function
    
    * Streamlined the draw functions
    
    * Added area as a polygon-level property in the geojson
    
    * Updated findAndCombinePolygons to handle situations with the same management but different efficacy/cost
    
    * Updated management area and cost calculations and gauge plot to take into account different management types and costs
    
    * Updated gauge plot to be more dynamic (display 'thousands','millions', etc.)
    
    * Rearranged items on the dashboard map.
    
    * Added new button to change displayed units
    
    * Updated draw tools panel with ability to change displayed area unit, efficacy and cost, circle tool with customizable radius
    
    * Removed console log from gauge plot
    
    * Added edit polygons functionality and extra customization to draw panel
    
    * Edit polygon box closes after the selection is deleted.
    
    * Removed redundant js to fetch run collection on click
    
    * Rearranged order of functions on dashboard, combined function to display/hide side panel
    
    * Moved polygon property change code to separate f()
    
    * Moved code to populate hidden fields for $/m2 and circle radius to separate functions
    
    * Removed unnecessary lines to update the cost and based on units
    
    * Moved functions related to drawing/editing polygons to a separate js file
    
    * Organized and formatted dashboard template and CSS styles
    
    * Trailing newline

---
+++ static_custom/js/dashboard/insertRunButton.js
@@ -0,0 +1,71 @@
+function insertRunButton(run_id, name, date, description, efficacy, infected_number, infected_area, cost) {
+  var run_pk = run_id.toString();
+  if (cost > max_cost) {
+    max_cost = cost;
+    console.log('Cost is greater' + max_cost);
+  };
+  if (infected_number > max_number) {
+    max_number = infected_number;
+    console.log('number_infected is greater' + max_number);
+  };
+  if (infected_area > max_area) {
+    max_area = infected_area;
+    console.log('infected_area is greater' + max_area);
+  };
+  console.log('Creating run button: ' + run_pk);
+  if (name.length > 18) {
+    var short_name = name.substring(0, 18) + '...';
+  } else {
+    short_name = name;
+  }
+  console.log('Short name: ' + short_name);
+  if (!(document.getElementById("run_" + run_pk))) {
+
+    $('<button id="run_button_' + run_pk +
+        '" type="button" class="btn text-right run-link pb-2" data-run-collection = "' + run_pk +
+        '">'+
+        '<a class="load_run_text" data-toggle="tooltip" title="Load results" data-placement="left">'+
+        '<i class="fas fa-play-circle"></i></a>' +
+        '<a class="run_info_button" tabindex="0" data-toggle="popover" title="<div><strong>' + name +
+        '</strong></div><small><em>' + date +
+        '</em></small>" data-content="<div>' + description +
+        '</div><div>Efficacy: ' + efficacy +
+        '</div>" data-placement="top"> <i class="fas fa-plus-circle"></i></a> '+
+        '<a class="delete_run_collection" data-toggle="tooltip" title="Delete" data-placement="left"><i class="fas fa-trash-alt"></i></a>' +
+        '<div id="run_' + run_pk +
+        '" class="comparison_plot" style="width:150px; height:120px;"></div></button>').prependTo($('#run-preview-container'));
+
+    $(function () {
+      $('[data-toggle="popover"]').popover({
+        boundary: 'viewport',
+        html: true,
+        trigger: 'hover focus',
+        template: '<div class="popover" role="tooltip"><div class="arrow"></div><div class="popover-header"></div><div class="popover-body"></div></div>'
+      })
+    })
+
+    $("#run_button_" + run_pk).on('click', function () {
+      $('#run-preview-container button').removeClass('active');
+      $(this).addClass('active');
+      var run_collection_id = $(this).attr('data-run-collection');
+      get_run_collection(run_collection_id);
+      console.log("Run collection" + run_id + "link clicked.");
+    });
+
+    $("#run_button_" + run_pk + ' a.load_run_text').on('click', function (event) {
+      event.stopPropagation();
+      console.log('Run button icon clicked.')
+      $(this).parent().trigger("click");
+      console.log('Triggered parent button click.')
+    });
+    $("#run_button_" + run_pk + ' a.run_info_button').on('click', function (event) {
+      event.stopPropagation();
+      console.log('Run info icon clicked.')
+    });
+    $('[data-toggle="tooltip"]').tooltip({
+      trigger: 'hover'
+    })
+  };
+  $(run_comparison_plot('run_' + run_pk, short_name, infected_number, infected_area, cost));
+
+  };
---
+++ static_custom/js/dashboard/spreadRatePlot.js
@@ -0,0 +1,109 @@
+function spreadRatePlot(last_year) {
+  var data = [];
+  console.log('Spread rate inside function: ')
+  console.log(spread_rate);
+  console.log(spread_rate.length);
+  var slider_year = parseInt($('#year-slider').find('input').val());
+  var year_count = parseInt($(".timeline-year-container.year_active").attr('data-year-counter'));
+  if (year_count == 0) {
+    year_color = 'grey';
+  } else {
+    year_color = colors[year_count - 1];
+  }
+  console.log(slider_year)
+
+  if (slider_year > last_year) {
+    $('#spread_rate_title span').text(slider_year);
+    for (var i = 0; i < spread_rate.length; i++) {
+      if (spread_rate[i].year == slider_year) {
+        console.log(spread_rate[i]);
+        data.push({
+          type: "scatterpolar",
+          r: [parseFloat(spread_rate[i].spreadrate__east_rate), parseFloat(spread_rate[i].spreadrate__north_rate), parseFloat(spread_rate[i].spreadrate__west_rate), parseFloat(spread_rate[i].spreadrate__south_rate), parseFloat(spread_rate[i].spreadrate__east_rate)],
+          theta: ["E", "N", "W", "S", "E"],
+          fill: "toself",
+          marker: {
+            color: year_color,
+            size: 2,
+          },
+          line: {
+            width: 2,
+          },
+        });
+      };
+    };
+  } else {
+    $('#spread_rate_title span').text('');
+    data.push({
+      type: "scatterpolar",
+      r: [0, 0, 0, 0],
+      theta: ["E", "N", "W", "S", "E"],
+      fill: "toself",
+      marker: {
+        color: 'grey',
+        size: 0,
+      },
+      line: {
+        width: 0,
+      },
+
+
+    });
+  }
+  console.log(data);
+  console.log('Max spread rate: ' + max_spread_rate);
+
+  var layout = {
+    font: {
+      size: 15,
+      color: "white",
+      border: "transparent"
+    },
+    showlegend: false,
+    legend: {
+      font: {
+        size: 16
+      }
+    },
+    polar: {
+      bgcolor: "#2e3236",
+      angularaxis: {
+        tickwidth: 2,
+        linewidth: 3,
+        showline: false,
+
+        layer: "below traces"
+      },
+      radialaxis: {
+        range: [0, max_spread_rate],
+        showline: false,
+        showticklabels: false,
+        showgrid: true,
+        zeroline: false,
+        autotick: true,
+        ticks: '',
+
+        visible: true,
+        linewidth: 2,
+        tickwidth: 2,
+        gridcolor: "#4A5158",
+        gridwidth: 2
+      }
+    },
+    orientation: 0,
+    paper_bgcolor: 'transparent',
+    plot_bgcolor: 'black',
+    margin: {
+      l: 40,
+      r: 40,
+      b: 50,
+      t: 30,
+      pad: 0
+    },
+  };
+
+  Plotly.newPlot('spread_plot', data, layout, {
+    displayModeBar: false,
+    responsive: true
+  });
+};
