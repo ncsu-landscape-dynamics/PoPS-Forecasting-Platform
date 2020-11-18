a676bff9973c1d2f58d5e9f8d70c1451599e61a9
shannonkjones <shankj687@gmail.com>
2020-11-18 08:41:39 -0500

    Implemented first chat server (synchronous)

--- pops_website/settings/base.py
+++ pops_website/settings/base.py
@@ -82,7 +82,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'pops_website.wsgi.application'
 ASGI_APPLICATION = 'pops_website.asgi.application'
-
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
