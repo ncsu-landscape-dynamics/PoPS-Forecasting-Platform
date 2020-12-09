a929e6cfb28390bb5df71a43a54fe8f946a3e0f2
ChrisJones687 <cjones1688@gmail.com>
2020-12-09 15:22:49 -0500

    added view for temp page and linted

--- pops/urls.py
+++ pops/urls.py
@@ -6,19 +6,19 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register('case_study', views.CaseStudyViewSet)
-#router.register('run', views.RunViewSet)
-router.register('run', views.RunModelWriteViewSet)
-router.register('run_collection', views.RunCollectionModelWriteViewSet)
-router.register('run_detail', views.RunDetailViewSet)
-router.register('run_collection_detail', views.RunCollectionDetailViewSet)
-#router.register('run_collection', views.RunCollectionViewSet)
-router.register('output', views.OutputViewSet)
-router.register('temperature_data', views.TemperatureDataViewSet)
-router.register('lethal_temperature_data', views.LethalTemperatureDataViewSet)
-router.register('precipitation_data', views.PrecipitationDataViewSet)
-router.register('session', views.SessionModelWriteViewSet)
-router.register('session_detail', views.SessionDetailViewSet)
+router.register("case_study", views.CaseStudyViewSet)
+# router.register('run', views.RunViewSet)
+router.register("run", views.RunModelWriteViewSet)
+router.register("run_collection", views.RunCollectionModelWriteViewSet)
+router.register("run_detail", views.RunDetailViewSet)
+router.register("run_collection_detail", views.RunCollectionDetailViewSet)
+# router.register('run_collection', views.RunCollectionViewSet)
+router.register("output", views.OutputViewSet)
+router.register("temperature_data", views.TemperatureDataViewSet)
+router.register("lethal_temperature_data", views.LethalTemperatureDataViewSet)
+router.register("precipitation_data", views.PrecipitationDataViewSet)
+router.register("session", views.SessionModelWriteViewSet)
+router.register("session_detail", views.SessionDetailViewSet)
 
 
 urlpatterns = [
@@ -36,7 +36,7 @@ urlpatterns = [
     # path('save_run_collection_data/', views.DashboardView.as_view(), name='save_run_collection_data'),
     # path('save_session_data/', views.NewSessionView.as_view(), name='save_session_data'),
     # path('save_run_collection_data_test/', views.DashboardTestView.as_view(), name='save_run_collection_data_test'),
-    #path('session/new/', views.NewSessionView.as_view(), name='new_session'),
+    # path('session/new/', views.NewSessionView.as_view(), name='new_session'),
     # path('session/new/<int:case_study>/', views.NewSessionView.as_view(), name='new_session_case_study_specific'),
     # path('session/delete/<int:pk>/', views.DeleteSessionView.as_view(), name='delete_session'),
     # path('session/list/', views.SessionListView.as_view(), name='session_list'),
@@ -47,8 +47,8 @@ urlpatterns = [
     # path('dashboard_layout', TemplateView.as_view(template_name="pops/dashboard/dashboard_layout.html")),
     # path('spotted_lanternfly/', TemplateView.as_view(template_name="pops/dashboard/spotted_lanternfly.html")),
     # path('workspace/', views.WorkspaceView.as_view(), name='workspace'),
-    path('pests/', views.PestListView.as_view(), name='pest_list'),
-    path('pest/<int:pk>/', views.PestDetailView.as_view(), name='pest_detail'),
+    path("pests/", views.PestListView.as_view(), name="pest_list"),
+    path("pest/<int:pk>/", views.PestDetailView.as_view(), name="pest_detail"),
     # path('dashboard/<int:pk>/', views.DashboardView.as_view(), name='dashboard'),
     # path('dashboard/', RedirectView.as_view(pattern_name='workspace'), name='dashboard_root'),
     # path('dashboard-test/<int:pk>/', views.DashboardTestView.as_view(), name='dashboard-test'),
@@ -61,7 +61,7 @@ urlpatterns = [
     # path('case_study/submitted/', views.case_study_submitted, name='case_study_submitted'),
     # path('cs/<int:pk>/', views.CaseStudyDetailView.as_view(), name='case-study'),
     # path('api/', include(router.urls)),
-    path('temp/', TemplateView.as_view(template_name="pops/dashboard/temp.html")),
-    #path('case_study/<int:pk>/edit', views.case_study_edit, name='case_study_edit'),
-    #path('myaccount', views.CaseStudyListView.as_view(), name='case-study-list'),
+    path("temp/", views.temp_view, name="temp"),
+    # path('case_study/<int:pk>/edit', views.case_study_edit, name='case_study_edit'),
+    # path('myaccount', views.CaseStudyListView.as_view(), name='case-study-list'),
 ]
