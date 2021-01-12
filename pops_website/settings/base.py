a1e6c6e78d36ba722031cb7bfc9c7c68425fb550
ChrisJones687 <cjones1688@gmail.com>
2021-01-12 16:36:17 -0500

    updated base.py for faster local dev

--- pops_website/settings/base.py
+++ pops_website/settings/base.py
@@ -34,12 +34,12 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django.contrib.gis",
     "rest_framework",
-    #"channels",
+    # "channels",
     "storages",
     "users.apps.UsersConfig",
     "pops.apps.PopsConfig",
     "team.apps.TeamConfig",
-    #"chat",
+    # "chat",
     "debug_toolbar",
 ]
 
@@ -139,27 +139,33 @@ USE_TZ = True
 
 # Moving static assets to DigitalOcean Spaces as per:
 # https://www.digitalocean.com/community/tutorials/how-to-set-up-object-storage-with-django
-AWS_ACCESS_KEY_ID = os.getenv("STATIC_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.getenv("STATIC_SECRET_KEY")
+USE_S3 = os.getenv("USE_S3", False) == "TRUE"
 
-AWS_STORAGE_BUCKET_NAME = os.getenv("STATIC_BUCKET_NAME")
-AWS_S3_ENDPOINT_URL = os.getenv("STATIC_ENDPOINT_URL")
-AWS_S3_CUSTOM_DOMAIN = os.getenv("STATIC_CDN_ENDPOINT_URL")
-AWS_S3_OBJECT_PARAMETERS = {
-    "CacheControl": "max-age=86400",
-}
-AWS_LOCATION = "static"
-AWS_DEFAULT_ACL = "public-read"
+if USE_S3:
+    AWS_ACCESS_KEY_ID = os.getenv("STATIC_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.getenv("STATIC_SECRET_KEY")
+    AWS_STORAGE_BUCKET_NAME = os.getenv("STATIC_BUCKET_NAME")
+    AWS_S3_ENDPOINT_URL = os.getenv("STATIC_ENDPOINT_URL")
+    AWS_S3_CUSTOM_DOMAIN = os.getenv("STATIC_CDN_ENDPOINT_URL")
+    AWS_S3_OBJECT_PARAMETERS = {
+        "CacheControl": "max-age=86400",
+    }
+    AWS_LOCATION = "static"
+    AWS_DEFAULT_ACL = "public-read"
+    STATIC_URL = "{}/{}/".format(AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+    STATIC_ROOT = "static/"
+    STATICFILES_STORAGE = "pops_website.custom_storages.StaticStorage"
+    DEFAULT_FILE_STORAGE = "pops_website.custom_storages.MediaStorage"
+    MEDIA_URL = "{}/{}/".format(AWS_S3_CUSTOM_DOMAIN, "media/")
+    MEDIA_ROOT = "media/"
+else:
+    STATIC_URL = "/static/"
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+    MEDIA_URL = "/media/"
+    MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-DEFAULT_FILE_STORAGE = "pops_website.custom_storages.MediaStorage"
-STATICFILES_STORAGE = "pops_website.custom_storages.StaticStorage"
 
-STATIC_URL = "{}/{}/".format(AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static_custom"),)
-STATIC_ROOT = "static/"
-
-MEDIA_URL = "{}/{}/".format(AWS_S3_CUSTOM_DOMAIN, "media/")
-MEDIA_ROOT = "media/"
 
 UPLOAD_ROOT = "media/uploads/"
 
