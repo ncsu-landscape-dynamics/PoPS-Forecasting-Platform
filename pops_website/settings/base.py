336a67eafa3abcb5375399284511fbb661b65d9e
shannonkjones <shankj687@gmail.com>
2021-03-19 12:40:34 -0400

    Recaptcha

--- pops_website/settings/base.py
+++ pops_website/settings/base.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+GOOGLE_RECAPTCHA_SECRET_KEY = os.getenv("RECAPTCHA_SECRET_KEY")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
