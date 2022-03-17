d6e0451d6b00bed303713724e91e015eb80200bc
ChrisJones687 <cjones1688@gmail.com>
2022-03-17 10:38:13 -0400

    created receivers.py with receivers

---
+++ pops/receivers.py
@@ -0,0 +1,53 @@
+import json
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import WebsocketConsumer, JsonWebsocketConsumer
+from django.contrib.auth.mixins import LoginRequiredMixin
+from channels.auth import login
+from .models import Run, RunCollection
+import channels.layers
+from django.dispatch import receiver
+from django.db.models import signals
+
+
+@receiver(signals.post_save, sender=RunCollection, weak=False)
+def new_run_collection(sender, instance, **kwargs):
+    print(sender)
+    print(instance)
+    print(instance.date_created)
+    layer = channels.layers.get_channel_layer()
+    group_name = "chat_%s" % instance.session.pk
+    data = {
+        "jsonrpc": "2.0",
+        "method": "new_run_collection",
+        "params": {
+            "run_collection": instance.pk,
+            "name": instance.name,
+            "date": instance.date_created.strftime("%B %d, %Y, %X"),
+            "description": instance.description,
+        },
+    }
+    async_to_sync(layer.group_send)(
+        group_name, {"type": "chat_message", "content": data}
+    )
+
+
+@receiver(signals.post_save, sender=Run, weak=False)
+def run_status_change(sender, instance, **kwargs):
+    print(sender)
+    print(instance)
+    print(instance.status)
+    print(instance.run_collection.session.pk)
+    layer = channels.layers.get_channel_layer()
+    group_name = "chat_%s" % instance.run_collection.session.pk
+    data = {
+        "jsonrpc": "2.0",
+        "method": "run_status_update",
+        "params": {
+            "run_pk": instance.pk,
+            "run_collection": instance.run_collection.pk,
+            "status": instance.status,
+        },
+    }
+    async_to_sync(layer.group_send)(
+        group_name, {"type": "chat_message", "content": data}
+    )
